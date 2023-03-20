f9dcf59db395770036fb826d6886f19ab585c1b4
BlackAlexander <37226910+BlackAlexander@users.noreply.github.com>
2023-03-20 22:41:17 +0200

    Add odds to bets

--- Assets/Misc/Code Generator/Codegenerator.py
+++ Assets/Misc/Code Generator/Codegenerator.py
@@ -1,11 +1,14 @@
 if __name__ == '__main__':
     f = open("input.txt", "r")
     baseString = f.read()
+    odds = ["0:0", "6:5", "7:9", "1:3", "3:7", "2:1", "1:19", "2:3", "1:2", "3:2", "5:7", "4:3", "8:3", "3:7", "2:11"]
     for i in range(1, 15):
         finalString = ""
         for character in baseString:
             if character == "P":
                 finalString += str(i)
+            elif character == "O":
+                finalString += odds[i]
             else:
                 finalString += character
         print(finalString + "\n")
